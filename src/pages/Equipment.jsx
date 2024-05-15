f10c5ac173e1df0f575460dd418b7e53e66b495e
Valentin Virzi <v.virzi@hotmail.com>
2024-05-15 20:15:44 -0300

    styling

--- src/pages/Equipment.jsx
+++ src/pages/Equipment.jsx
@@ -7,47 +7,50 @@ const Equipment = () => {
       weight: 25,
       units: 13,
       massUnit: "Kg", // TODO: Remove this property from the objects and make it a universal variable from a useState() managed from the Settings subpage.
-      color: "bg-red-800",
+      color: "bg-red-800", // TODO: Make a color picker for the PlateForm element and insert the choice on each object.
     },
     {
       weight: 25,
       units: 13,
       massUnit: "Kg", // TODO: Remove this property from the objects and make it a universal variable from a useState() managed from the Settings subpage.
-      color: "bg-violet-800",
+      color: "bg-violet-800", // TODO: Make a color picker for the PlateForm element and insert the choice on each object.
     },
     {
       weight: 25,
       units: 13,
       massUnit: "Kg", // TODO: Remove this property from the objects and make it a universal variable from a useState() managed from the Settings subpage.
-      color: "bg-green-800",
+      color: "bg-green-800", // TODO: Make a color picker for the PlateForm element and insert the choice on each object.
     },
     {
       weight: 25,
       units: 13,
       massUnit: "Kg", // TODO: Remove this property from the objects and make it a universal variable from a useState() managed from the Settings subpage.
-      color: "bg-blue-800",
+      color: "bg-blue-800", // TODO: Make a color picker for the PlateForm element and insert the choice on each object.
     },
     {
       weight: 25,
       units: 13,
       massUnit: "Kg", // TODO: Remove this property from the objects and make it a universal variable from a useState() managed from the Settings subpage.
-      color: "bg-yellow-600",
+      color: "bg-yellow-600", // TODO: Make a color picker for the PlateForm element and insert the choice on each object.
     },
   ];
   return (
     <div className="flex min-h-screen w-full flex-col items-center bg-black">
       <Header title="Equipment" />
       {/* TODO: Start making the main pages for the app */}
-      <main className="mt-4 flex w-full flex-col items-center gap-4 pb-24 pt-24 md:pb-32 lg:pb-36">
-        <div className="flex flex-col items-center gap-2 text-white">
+      <main className="mt-4 flex w-full flex-col items-center gap-4 pb-24 pt-20 md:pb-32 md:pt-24 lg:pb-36">
+        <div className="flex flex-col items-center gap-2 pt-4 text-white">
           <h2 className="text-3xl font-semibold">Available Plates</h2>
           <p className="text-base">( Tap plates to edit )</p>
         </div>
         <div className="grid w-full grid-cols-3 place-content-center justify-items-center gap-2 gap-y-5 p-4 text-white md:w-11/12 md:gap-3 md:gap-y-6 xl:w-8/12">
-          {/* TODO: When the gap of the buttons collapses the sizes break, so i have to adjust properties if the screen is too small */}
-          {AvailablePlates.map((plate) => (
+          {/*
+          When the gap of the buttons collapses the sizes break, so i have to adjust properties if the screen is too small.
+          Be careful with the padding of the parent element to not collapse too.
+          */}
+          {AvailablePlates.map((plate, index) => (
             <PlateEquipment
-              key={plate.index}
+              key={index}
               weight={plate.weight}
               units={plate.units}
               massUnit={plate.massUnit}
